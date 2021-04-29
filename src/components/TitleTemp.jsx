216407782037a984cead426c5afd5fc0f00edb29
Mardoxhee <MardoxheeLuviki@gmail.com>
2021-04-29 09:15:50 +0100

    pose de la première pierre

---
+++ src/components/TitleTemp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TitleTemp() {
+    return (
+        <div>
+            <h1>ICI SERA ERIGE UN TRES GRAND SITE DE CINEMA</h1>
+            <h2>TECHNOLOGIE FRONT : REACT JS</h2>
+        </div>
+    )
+}
+
+export default TitleTemp
